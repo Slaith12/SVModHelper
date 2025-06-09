af57aec7dff2dd4feeea743666fa07e4e1e80618
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2025-06-08 22:17:53 -0700

    Initial commit

---
+++ SVModHelper/Core.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(SVModHelper.Core), "SVModHelper", "1.0.0", "Slaith", null)]
+[assembly: MelonGame("Pengonauts", "StarVaders")]
+[assembly: HarmonyDontPatchAll]
+
+namespace SVModHelper
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
