4431f5fb667fca93af14aa0d15d0541d9caf7ee6
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2026-08-19 00:51:27 -0700

    Add PrintAll___Names functions to GameDebug.cs
    Added functions to print all modded card/artifact names to easily go along with the AddCard/Artifact functions. Also added extra console messages and comments to clarify how this class works.

--- SVModHelper/GameDebug.cs
+++ SVModHelper/GameDebug.cs
@@ -1,7 +1,13 @@
-﻿using MelonLoader;
+﻿using Il2CppStarVaders;
+using MelonLoader;
+using SVModHelper.ModContent;
 
 namespace SVModHelper
 {
+    /// <summary>
+    /// <para>This class is intended to be used with the UnityExplorer mod (https://github.com/yukieiji/UnityExplorer)
+    /// to get various debug options while playing.</para>
+    /// </summary>
     public static class GameDebug
     {
         public static void AddArtifact(int artifact)
@@ -13,10 +19,21 @@ namespace SVModHelper
         {
             if(!DataManager.PlayerHasRunInProgress())
             {
-                Melon<Core>.Logger.Error("No run in progress.");
+                Melon<Core>.Logger.Error("Cannot use AddArtifact() when there's no run in progress.");
                 return;
             }
+            Melon<Core>.Logger.Msg($"Adding artifact {artifact} to campaign deck (does not affect current encounter if active)");
             DataManager.PlayerData.AddArtifact(artifact);
+        }
+
+        public static void PrintAllModArtifactNames()
+        {
+            Melon<Core>.Logger.Msg("Calling PrintAllModArtifactNames:");
+            foreach(ArtifactName name in ModContentManager.GetAllModArtifactNames())
+            {
+                IHasArtifactID artifact = ModContentManager.GetModArtifactInstance(name);
+                Melon<Core>.Logger.Msg($"{name}: {artifact.DisplayName} ({artifact.ID})");
+            }
         }
 
         public static void AddCardToDeck(int card, int component = 0, int affinity = 0)
@@ -28,15 +45,21 @@ namespace SVModHelper
         {
             if (!DataManager.PlayerHasRunInProgress())
             {
-                Melon<Core>.Logger.Error("No run in progress.");
+                Melon<Core>.Logger.Error("Cannot use AddCardToDeck() when there's no run in progress.");
                 return;
             }
-            if (!DataManager.PlayerHasRunInProgress())
-            {
-                Melon<Core>.Logger.Error("No run in progress.");
-                return;
-            }
+            Melon<Core>.Logger.Msg($"Adding card {card} to campaign deck (does not affect current encounter if active)");
             DataManager.PlayerData.AddCardToDeck(new PlayerCardData(card, component, affinity));
+        }
+
+        public static void PrintAllModCardNames()
+        {
+            Melon<Core>.Logger.Msg("Calling PrintAllModCardNames:");
+            foreach (CardName name in ModContentManager.GetAllModCardNames())
+            {
+                AModCard card = ModContentManager.GetModCardInstance(name);
+                Melon<Core>.Logger.Msg($"{name}: {card.DisplayName} ({card.ID})");
+            }
         }
     }
 }
