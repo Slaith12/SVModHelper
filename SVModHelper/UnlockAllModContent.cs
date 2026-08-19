df29c791219f553ce20c9922e11d480d1cf027d9
Slaith12 <34562469+Slaith12@users.noreply.github.com>
2026-08-19 00:38:17 -0700

    Create UnlockAllModContent.cs
    This script makes it so the "Skip Unlocks and Pilots" button reveals all custom cards/artifacts in the compendium.
    
    I don't really have any excuse for not implementing this earlier. It was really easy, I never thought it'd be difficult, and it's really useful for checking your card's appearance/description.

---
+++ SVModHelper/UnlockAllModContent.cs
@@ -0,0 +1,12 @@
+﻿namespace SVModHelper
+{
+    [HarmonyPatch(typeof(LoggerDataSO), nameof(LoggerDataSO.UnlockEverything))]
+    internal static class UnlockAllModContent
+    {
+        private static void Postfix(LoggerDataSO __instance)
+        {
+            __instance.DiscoveredCards.UnionWith(ModContentManager.GetAllModCardNames().ToILCPPEnumerable());
+            __instance.DiscoveredArtifacts.UnionWith(ModContentManager.GetAllModArtifactNames().ToILCPPEnumerable());
+        }
+    }
+}
